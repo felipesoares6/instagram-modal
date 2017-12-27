9be76cc7284ca190a02b869b21d6e173cd7c30f7
felipe.soares <felipeluizsoares@gmail.com>
2017-12-27 20:37:21 -0200

    Update Content modal style according with to original

--- src/components/modal/Content.js
+++ src/components/modal/Content.js
@@ -3,17 +3,15 @@ import styled from 'styled-components'
 
 const ModalStyled = styled.div`
   display: ${ props => props.visible ? 'block' : 'none' };
-  width: 600px;
+  width: 900px;
   max-width: 90%;
-  height: 400px;
+  height: 450px;
   max-height: 90%;
   position: fixed;
   z-index: 100;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  background: #fff;
-  box-shadow: 0 0 1px 1px rgba(0,0,0,.2);
 `
 
 const Content = ({ visible, children }) => (
