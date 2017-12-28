e60c4b0f0e638707749bdb4c10f83e8d230694be
felipe.soares <felipeluizsoares@gmail.com>
2017-12-27 22:31:07 -0200

    Add Profile component with User

---
+++ src/components/modal-photo/User.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import avatar from '../../images/avatar.png'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const Photo = styled.img`
+  background-image: url(${ avatar });
+  background-position: center;
+  background-size: cover;
+  height: 40px;
+  width: 40px;
+  border-radius: 150%;
+`
+
+const Name = styled.h1`
+  letter-spacing: 1px;
+  font-size: .875rem;
+  margin-left: 10px;
+`
+
+const User = () => (
+  <Container>
+    <Photo src={ avatar } />
+    <Name> felipesoares </Name>
+  </Container>
+)
+
+export default User
