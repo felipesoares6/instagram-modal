c7327662a2e5f922aef42026a0e4d6fb3c8dd33c
felipe.soares <felipeluizsoares@gmail.com>
2017-12-28 19:50:49 -0200

    Update Modal Structure component to render the arrows

--- src/components/modal/Structure.js
+++ src/components/modal/Structure.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { Arrow, ArrowLeft } from './Arrow'
+
 const StructureStyled = styled.div`
-  display: ${ props => props.visible ? 'block' : 'none' };
-  width: 850px;
-  max-width: 95%;
-  height: 550px;
-  max-height: 90%;
+  display: ${ props => props.visible ? 'flex' : 'none' };
+  width: 102%;
+  min-height: 100%;
   position: fixed;
   z-index: 100;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -48%);
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    transform: translateY(-50%, -50%);
+    width: 950px;
+    min-width: 95%;
+    height: 500px;
+    min-height: 80%;
+  }
 `
 
 const Structure = ({ visible, children }) => (
   <StructureStyled visible={ visible }>
+    <ArrowLeft visible={ visible } />
+
     { children }
+
+    <Arrow visible={ visible } />
   </StructureStyled>
 )
 
